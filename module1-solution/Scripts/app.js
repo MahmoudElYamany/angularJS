ac8ff6b9c329a2745d7d9565b4a48b9136c0e12f
MahmoudELYamany <mahmoud261111@gmail.com>
2021-07-04 13:26:16 +0200

    make the assignment

---
+++ module1-solution/Scripts/app.js
@@ -0,0 +1,32 @@
+(function () {
+    'use strict';
+
+    angular.module('LunchCheck', [])
+    .controller('LunchCheckController',LunchCheckController )
+
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope) {
+        $scope.list="";
+        $scope.message="";
+
+        $scope.apply = function(){
+            if($scope.list == ""){
+                $scope.message="Please enter data first"
+                document.getElementById('message').style.color="red"
+                document.getElementById('message').style.border="solid red"
+            }
+            else{
+                var listitem = $scope.list.split(',').length;
+                if(listitem<=3){
+                    $scope.message="Enjoy!"
+                    document.getElementById('message').style.color="green"
+                    document.getElementById('message').style.border="solid green"
+                    
+                }
+                else{
+                    $scope.message="Too much!"
+                }
+            }
+        }
+    }
+})()
